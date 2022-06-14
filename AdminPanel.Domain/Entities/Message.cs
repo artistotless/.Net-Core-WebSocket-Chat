5971a8280aff949b46b70531acf15de33aa3824b
Aslan Bozieff <neuralink7232050@gmail.com>
2022-06-14 19:22:07 +0300

    deleted trash Files

--- AdminPanel.Domain/Entities/Message.cs
+++ AdminPanel.Domain/Entities/Message.cs
@@ -4,7 +4,7 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace AdminPanel.Domain.Entities
 {
-    public class Message: IEqualityComparer<Message>
+    public class Message
     {
         public int MessageID { get; set; }
         public string Text { get; set; }
@@ -14,15 +14,5 @@ namespace AdminPanel.Domain.Entities
         public Player Player { get; set; }
         public int? OperatorID { get; set; }
         public Operator Operator { get; set; }
-
-        public bool Equals(Message x, Message y)
-        {
-           return x.PlayerID == y.PlayerID;
-        }
-
-        public int GetHashCode([DisallowNull] Message obj)
-        {
-            return obj.PlayerID.GetHashCode();
-        }
     }
 }
